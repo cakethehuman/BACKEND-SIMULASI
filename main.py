cf57c8a09bd4f50cb22d901da572d6b8802e4eb0
NormTryToCode <wijayachandra160@gmail.com>
2026-02-25 10:40:53 +0700

    hello semuaaa

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello world")
-print("line 2 nih bos")
+import turtle
+
+myTurtle = turtle.Turtle()
+
+myScreen = turtle.Screen()
+
+
+
+myTurtle.write(arg="Hello, World!", move=True, font=("JetBrains Mono Nerd Font", 20, ("bold", "italic")))
+
+myScreen.exitonclick()
